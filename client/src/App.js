8b8cdaff40eaea5c7a276d5b5885c7a010ab349d
cnb1 <hackerrank134@gmail.com>
2022-04-10 12:46:07 -0400

    front end basic

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import { Container } from 'semantic-ui-react';
+
 
 function App() {
   return (
-   <div>
-     <h1>Hi world!</h1>
-  </div>
+     <Router>
+      
+      <Container>
+        <MenuBar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/login' element = {<Login/>}/>
+          <Route exact path='/register' element = {<Register/>}/>
+        </Routes>
+      </Container>
+      
+    </Router>
   );
 }
 
