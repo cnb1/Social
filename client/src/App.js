64019fab75dcc04f46b63e0644023a79cd0ec279
cnb1 <hackerrank134@gmail.com>
2022-04-28 13:30:19 -0400

    added auth router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 import { AuthProvider } from './context/auth';
+import  AuthRoute from './util/AuthRoute'; 
 
 import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
@@ -20,8 +21,12 @@ function App() {
           <MenuBar />
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<AuthRoute />}>
+              <Route exact path='/login' element={<Login/>}/>
+            </Route>
+            <Route exact path="/register" element={<AuthRoute />}>
+              <Route exact path='register' element={<Register/>}/>
+            </Route>
           </Routes>
         </Container>
 
--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
+  const {user, logout} = useContext(AuthContext);
 
   const pathname = window.location.pathname;
 
@@ -11,35 +14,57 @@ function MenuBar() {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-
-  return (
-    <Menu pointing secondary size="massive" color="teal">
-      <Menu.Item
-        name='home'
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-        as={Link}
-        to="/"
-      />
-
-      <Menu.Menu position='right'>
+  const menubar = user ? (
+     
+      <Menu pointing secondary size="massive" color="teal">
         <Menu.Item
-          name='login'
-          active={activeItem === 'login'}
+          name={user.username}
+          active
+          as={Link}
+          to="/"
+        />
+  
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='logout'
+            onClick={logout}
+          />
+          
+        </Menu.Menu>
+      </Menu>
+     
+  ) : (
+    (
+      <Menu pointing secondary size="massive" color="teal">
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
           onClick={handleItemClick}
           as={Link}
-          to="/login"
+          to="/"
         />
-        <Menu.Item
-          name='register'
-          active={activeItem === 'register'}
-          onClick={handleItemClick}
-          as={Link}
-          to="/register"
-        />
-      </Menu.Menu>
-    </Menu>
-  );
+  
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='login'
+            active={activeItem === 'login'}
+            onClick={handleItemClick}
+            as={Link}
+            to="/login"
+          />
+          <Menu.Item
+            name='register'
+            active={activeItem === 'register'}
+            onClick={handleItemClick}
+            as={Link}
+            to="/register"
+          />
+        </Menu.Menu>
+      </Menu>
+    )
+  )
+
+  return menubar;
 }
 
 
